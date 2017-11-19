767e20e1d57d45af0f763ed0e0c0f42706edd7fa
Kyle Grenell <kyle.grenell@gmail.com>
2017-11-19 13:22:21 +0000

    setup browser sync server in gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-postcssimport = require('postcss-import');
+postcssimport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 gulp.task('default', function(){
   console.log("gulp task working");
@@ -22,8 +23,14 @@ gulp.task('styles', function(){
 
 gulp.task('watch', function(){
 
+  browserSync.init({
+    server: {
+      baseDir: "app"
+    }
+  });
+
   watch('./app/index.html', function(){
-    gulp.start('html');
+    browserSync.reload();
   });
 
   watch('./app/assets/styles/**/*.css',function(){
