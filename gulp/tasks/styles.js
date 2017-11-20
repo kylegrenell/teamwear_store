1bc3a95735e4bc4e6b5db5243ad7bfa491ae1185
Kyle Grenell <kyle.grenell@gmail.com>
2017-11-20 13:43:28 +0000

    mixins for media queries added

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,12 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-postcssimport = require('postcss-import');
+postcssimport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([postcssimport, cssvars, nested, autoprefixer]))
+    .pipe(postcss([postcssimport, mixins, cssvars, nested, autoprefixer]))
     .on('error', function(errInfo){ //stop gulp watch crashing
       console.log(errInfo.toString());
       this.emit('end');
