2e420dd605c19c28be4e115f9191213fdea225a5
Kyle Grenell <kyle.grenell@gmail.com>
2017-11-20 12:28:54 +0000

    styling and html nav pages added

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+postcssimport = require('postcss-import');
+
+gulp.task('styles', function(){
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([postcssimport, cssvars, nested, autoprefixer]))
+    .on('error', function(errInfo){ //stop gulp watch crashing
+      console.log(errInfo.toString());
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
+});
