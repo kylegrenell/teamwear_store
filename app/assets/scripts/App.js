29394ff48eb294b32473f16f81d8a199d3431f96
Kyle Grenell <kyle.grenell@gmail.com>
2017-11-30 18:04:56 +0000

    console log easter egg colours, minor css changes

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,4 +4,4 @@ import RevealOnScroll from './modules/RevealOnScroll';
 var mobileMenu = new MobileMenu();
 var revealOnScroll = new RevealOnScroll();
 
-console.log("Psst, if you're seeing this, check back from time to time for secret discounts... This is our 'Easter Egg' section :) ")
+console.log("%c Psst, if you're seeing this, check back from time to time for secret discounts... This is our 'Easter Egg' section :)", "background: #ffc6df; color: #9c8ac1");
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -10343,7 +10343,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var mobileMenu = new _MobileMenu2.default();
 var revealOnScroll = new _RevealOnScroll2.default();
 
-console.log("Psst, if you're seeing this, check back from time to time for secret discounts... This is our 'Easter Egg' section :) ");
+console.log("%c Psst, if you're seeing this, check back from time to time for secret discounts... This is our 'Easter Egg' section :)", "background: #ffc6df; color: #9c8ac1");
 
 /***/ }),
 /* 2 */
