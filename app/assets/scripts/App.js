81ac94e78395562041adada7ef4bb13cd0e578e6
Kyle Grenell <kyle.grenell@gmail.com>
2017-12-01 17:11:32 +0000

    moved instagram follow to new div

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,6 +5,6 @@ import $ from 'jquery';
 var mobileMenu = new MobileMenu();
 // var revealOnScroll = new RevealOnScroll();
 new RevealOnScroll($(".footer-wrapper"), "85%");
-new RevealOnScroll($(".squint-logo-image"), "85%");
+new RevealOnScroll($(".squint-logo-image"), "90%");
 
 console.log("%c Psst, if you're seeing this, check back from time to time for secret discounts... This is our 'Easter Egg' section :)", "background: #ffc6df; color: #9c8ac1");
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -10347,7 +10347,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var mobileMenu = new _MobileMenu2.default();
 // var revealOnScroll = new RevealOnScroll();
 new _RevealOnScroll2.default((0, _jquery2.default)(".footer-wrapper"), "85%");
-new _RevealOnScroll2.default((0, _jquery2.default)(".squint-logo-image"), "85%");
+new _RevealOnScroll2.default((0, _jquery2.default)(".squint-logo-image"), "90%");
 
 console.log("%c Psst, if you're seeing this, check back from time to time for secret discounts... This is our 'Easter Egg' section :)", "background: #ffc6df; color: #9c8ac1");
 
