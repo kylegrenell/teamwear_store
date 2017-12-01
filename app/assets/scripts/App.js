2fcc2267565651c50c17353f90f00980e6160962
Kyle Grenell <kyle.grenell@gmail.com>
2017-12-01 14:49:06 +0000

    reusable reveal on scroll js added, css changes to header on html pages

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
-var revealOnScroll = new RevealOnScroll();
+// var revealOnScroll = new RevealOnScroll();
+new RevealOnScroll($(".footer-wrapper"), "85%");
+new RevealOnScroll($(".squint-logo-image"), "85%");
 
 console.log("%c Psst, if you're seeing this, check back from time to time for secret discounts... This is our 'Easter Egg' section :)", "background: #ffc6df; color: #9c8ac1");
---
+++ gulp/tasks/build.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+
+gulp.task('optimizeImages', function(){
+  
+})
+
+gulp.task('build', []);
