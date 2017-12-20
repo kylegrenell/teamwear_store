1cec634df6ab3cb79aa9aacf462eac9d22d5e1a6
Kyle Grenell <kyle.grenell@gmail.com>
2017-12-20 13:28:22 +0000

    all changes client request

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -10,18 +10,3 @@ new RevealOnScroll($(".footer-wrapper"), "85%");
 new RevealOnScroll($(".squint-logo-image"), "90%");
 
 console.log("%c Psst, if you're seeing this, check back from time to time for secret discounts... This is our 'Easter Egg' section :)", "background: #ffc6df; color: #9c8ac1");
-
-// var userFeed = new Instafeed({
-//   get: 'user',
-//   userId: '3322396718',
-//   clientId: 'a8174865bc804521b7b8cf547bbabb48',
-//   accessToken: '3322396718.a817486.ad1ad8469ed0449393089995d0fa2cb8',
-//   resolution: 'standard_resolution',
-//   template: '<div class="col-lg-3 col-sm-3 col-xs-4"><a href="{{link}}"><img class="img-responsive" src="{{image}}" /></a></div>',
-//   sortBy: 'most-recent',
-//   limit: 8,
-//   links: true
-// });
-
-// userFeed.run();
-
